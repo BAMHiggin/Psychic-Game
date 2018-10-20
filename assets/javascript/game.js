c6f8b20f037c0e27a3e064e37a50ad78a9615224
BAMHiggin <bamhiggin@gmail.com>
2018-10-19 23:31:38 -0500

    added logic for losses, limited to a-z letters only, eliminated duplicates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,6 +12,47 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 function randomLetter() {
     return ('abcdefghijklmnopqrstuvwxyz').split('')[(Math.floor(Math.random() * 26))];
 }
+function varReset() {
+    guessedLetters = []; // reset to blank guesses
+    rLetter = randomLetter(); // computer picks new letter
+    guesses = 7; // guests reset 
+}
+
+function isLetter(letter) {
+
+    if (letters.indexOf(letter) >= 0) { //checking letter array to make sure that only strings with letters a-z are passed- if no index then not in letters
+        return true;
+    }
+    else {
+
+        return false;
+    }
+}
+
+function isDuplicateGuess(letter) {
+
+
+    if (guessedLetters.indexOf(letter) >= 0) { //checking if guessed letter was entered twice
+        return true;
+    }
+    else {
+
+        return false;
+    }
+}
+
+
+
+function logAllVars() {
+    console.log("computer generated random letter: " + rLetter);
+    console.log("wins: " + wins);
+    console.log("losses: " + losses);
+    console.log("guesses: " + guesses);
+    console.log("guessed letters--");
+    guessedLetters.forEach(element => {
+        console.log(element);
+    });
+}
 
 // setting random letter
 var rLetter = randomLetter();
@@ -19,10 +60,24 @@ var rLetter = randomLetter();
 /// when a key is pressed, run game logic
 document.onkeyup = function (event) {
     var inputLetter = event.key.toLowerCase();
-    if (rLetter === inputLetter) {
-        wins++; // +1 win
-        guessedLetters = []; // reset to blank guesses
-        rLetter = randomLetter(); // computer picks new letter
+    if (isLetter(inputLetter) && !isDuplicateGuess(inputLetter)) { // checking if input is an a-z, and not a duplicate guess
+        if (rLetter === inputLetter) {
+            wins++; // +1 win
+            varReset();
+        }
+        if (rLetter !== inputLetter) {
+            guesses--; // one less guess
+            if (guesses < 1) {
+                losses++; // +1 loss
+                varReset();
+            }
+            else {
+                guessedLetters.push(inputLetter);
+            }
+
+
+        }
+        logAllVars();
     }
 }
 
